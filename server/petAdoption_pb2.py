03bd0947474649bb692f756db6f3c70099d4b575
uta1002092849 <mmt2849@mavs.uta.edu>
2024-10-08 16:55:29 -0500

    commit new generated python code

--- server/petAdoption_pb2.py
+++ server/petAdoption_pb2.py
@@ -24,14 +24,14 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11petAdoption.proto\x12\x0bpetAdoption\"\\\n\x03Pet\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06gender\x18\x03 \x01(\t\x12\x0b\n\x03\x61ge\x18\x04 \x01(\x05\x12\r\n\x05\x62reed\x18\x05 \x01(\t\x12\x0f\n\x07picture\x18\x06 \x01(\x0c\"]\n\x10InsertPetRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0e\n\x06gender\x18\x02 \x01(\t\x12\x0b\n\x03\x61ge\x18\x03 \x01(\x05\x12\r\n\x05\x62reed\x18\x04 \x01(\t\x12\x0f\n\x07picture\x18\x05 \x01(\x0c\"$\n\x11InsertPetResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\"&\n\x10SearchPetRequest\x12\x12\n\nsearchTerm\x18\x01 \x01(\t\"3\n\x11SearchPetResponse\x12\x1e\n\x04pets\x18\x01 \x03(\x0b\x32\x10.petAdoption.Pet2\xa5\x01\n\x0bPetAdoption\x12J\n\tInsertPet\x12\x1d.petAdoption.InsertPetRequest\x1a\x1e.petAdoption.InsertPetResponse\x12J\n\tSearchPet\x12\x1d.petAdoption.SearchPetRequest\x1a\x1e.petAdoption.SearchPetResponseB\x10Z\x0ego/petAdoptionb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x11petAdoption.proto\x12\x0bpetAdoption\"\\\n\x03Pet\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06gender\x18\x03 \x01(\t\x12\x0b\n\x03\x61ge\x18\x04 \x01(\x05\x12\r\n\x05\x62reed\x18\x05 \x01(\t\x12\x0f\n\x07picture\x18\x06 \x01(\x0c\"]\n\x10InsertPetRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0e\n\x06gender\x18\x02 \x01(\t\x12\x0b\n\x03\x61ge\x18\x03 \x01(\x05\x12\r\n\x05\x62reed\x18\x04 \x01(\t\x12\x0f\n\x07picture\x18\x05 \x01(\x0c\"$\n\x11InsertPetResponse\x12\x0f\n\x07success\x18\x01 \x01(\x08\"&\n\x10SearchPetRequest\x12\x12\n\nsearchTerm\x18\x01 \x01(\t\"3\n\x11SearchPetResponse\x12\x1e\n\x04pets\x18\x01 \x03(\x0b\x32\x10.petAdoption.Pet2\xa5\x01\n\x0bPetAdoption\x12J\n\tInsertPet\x12\x1d.petAdoption.InsertPetRequest\x1a\x1e.petAdoption.InsertPetResponse\x12J\n\tSearchPet\x12\x1d.petAdoption.SearchPetRequest\x1a\x1e.petAdoption.SearchPetResponseB\x0fZ\r./petAdoptionb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'petAdoption_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
-  _globals['DESCRIPTOR']._serialized_options = b'Z\016go/petAdoption'
+  _globals['DESCRIPTOR']._serialized_options = b'Z\r./petAdoption'
   _globals['_PET']._serialized_start=34
   _globals['_PET']._serialized_end=126
   _globals['_INSERTPETREQUEST']._serialized_start=128
