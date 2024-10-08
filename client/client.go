8b744520c9dbc11b95d8e5a2b3f8e6a3148a9b0a
uta1002092849 <mmt2849@mavs.uta.edu>
2024-10-08 16:27:59 -0500

    upadate client to handle image posting

--- client/client.go
+++ client/client.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"strconv"
 	"time"
 
 	pb "github.com/packer/petAdoption"
@@ -16,52 +21,114 @@ var (
 )
 
 func main() {
-
 	flag.Parse()
-	// Set up a connection to the server.
+
+	// Set up a connection to the gRPC server.
 	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("Could not connect to the server with the following error code: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewPetAdoptionClient(conn)
+	client := pb.NewPetAdoptionClient(conn)
+
+	// Set up HTTP server
+	http.HandleFunc("/", serveHTML)
+	http.HandleFunc("/insert-pet", func(w http.ResponseWriter, r *http.Request) {
+		handleInsertPet(w, r, client)
+	})
+	http.HandleFunc("/search-pet", func(w http.ResponseWriter, r *http.Request) {
+		handleSearchPet(w, r, client)
+	})
+
+	fmt.Println("Server is running on http://localhost:8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func handleInsertPet(w http.ResponseWriter, r *http.Request, client pb.PetAdoptionClient) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse the multipart form data
+	err := r.ParseMultipartForm(10 << 20) // 10 MB max
+	if err != nil {
+		http.Error(w, "Unable to parse form: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Get form values
+	name := r.FormValue("name")
+	gender := r.FormValue("gender")
+	ageStr := r.FormValue("age")
+	breed := r.FormValue("breed")
+
+	// Convert age to int32
+	age, err := strconv.ParseInt(ageStr, 10, 32)
+	if err != nil {
+		http.Error(w, "Invalid age: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Get the picture file
+	file, _, err := r.FormFile("picture")
+	if err != nil {
+		http.Error(w, "Error retrieving the file: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Read the file content
+	pictureData, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Error reading the file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	// Define a new pet to insert
-	pet_request := &pb.InsertPetRequest{
-		Name:   "Alabdabraka",
-		Gender: "IDK",
-		Age:    22,
-		Breed:  "Mutt",
+	petRequest := &pb.InsertPetRequest{
+		Name:    name,
+		Gender:  gender,
+		Age:     int32(age),
+		Breed:   breed,
+		Picture: pictureData,
 	}
 
-	// Call the translate service
-	r, err := c.InsertPet(ctx, pet_request)
+	response, err := client.InsertPet(ctx, petRequest)
 	if err != nil {
-		log.Fatalf("could not call insert pet: %v", err)
+		http.Error(w, "Could not insert pet: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	if r.Success {
-		log.Printf("Pet inserted successfully")
-	} else {
-		log.Printf("Pet insertion failed")
+	json.NewEncoder(w).Encode(map[string]bool{"success": response.Success})
+}
+
+func handleSearchPet(w http.ResponseWriter, r *http.Request, client pb.PetAdoptionClient) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
-	// search for pet
-	// r, err := c.SearchPet(ctx, &pb.SearchPetRequest{SearchTerm: "dog"})
+	searchTerm := r.URL.Query().Get("term")
+	if searchTerm == "" {
+		http.Error(w, "Search term is required", http.StatusBadRequest)
+		return
+	}
 
-	// if err != nil {
-	// 	log.Printf("Coulnt search for pet")
-	// }
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-	// for index, pet := range r.Pets {
-	// 	fmt.Println("Index:", index)
-	// 	fmt.Println("\tPet ID:", pet.Id)
-	// 	fmt.Println("\tPet Name:", pet.Name)
-	// 	fmt.Println("\tPet Gener:", pet.Gender)
-	// 	fmt.Println("\tPet Gener:", pet.Breed)
-	// 	fmt.Println("\tPet Picture:", pet.Picture)
-	// }
+	response, err := client.SearchPet(ctx, &pb.SearchPetRequest{SearchTerm: searchTerm})
+	if err != nil {
+		http.Error(w, "Could not search for pets: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	json.NewEncoder(w).Encode(map[string][]*pb.Pet{"pets": response.Pets})
 }
